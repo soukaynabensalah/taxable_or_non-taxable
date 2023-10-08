5f4b80a0148d1c95928040db43f981cb889c519d
soukaynabensalah <146835722+soukaynabensalah@users.noreply.github.com>
2023-10-08 23:52:41 +0100

    Create Tax.py

---
+++ Tax.py
@@ -0,0 +1,8 @@
+sexe=str(input("entrer le sexe"))#f pour la femme et h pour l'homme
+age=int(input("entrer l'age"))
+if (sexe=h and age>=20) or (sexe=f and age>=18 and age<=35) :
+   print("imposable")
+else :
+   print ("non imposable")
+  
+
